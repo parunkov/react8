2d4ec00eba374f7189079affd86b7654d7cf3700
parunkov <parunkov@gmail.com>
2023-07-28 13:09:01 +0500

    request

--- src/componets/Page/Page.jsx
+++ src/componets/Page/Page.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Page = () => {
+    const [users, setUsers] = useState([]);
+
+    const onButtonClick = async () => {
+        const response = await fetch('https://api.github.com/search/users?q=tom&sort=repositories&order=asc');
+        const dataUsers = await response.json();
+        setUsers(dataUsers.items);
+    }
     return (
         <div className="container">
             <h1 className="title">AAAA</h1>
+            <button onClick={onButtonClick}>Найти</button>
+            {users.map((user) => (
+                <div className="user" key={user.id}>{user.login}</div>
+            ))}
         </div>
     )
 }
